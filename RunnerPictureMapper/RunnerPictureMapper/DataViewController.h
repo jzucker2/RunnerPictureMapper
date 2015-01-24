6e3fb0fb93f01837bb68f34586a0b162fb916bcf
Jordan Zucker <jordan@ffrree.com>
2015-01-23 22:12:03 -0800

    Added initial project.

---
+++ RunnerPictureMapper/RunnerPictureMapper/DataViewController.h
@@ -0,0 +1,17 @@
+//
+//  DataViewController.h
+//  RunnerPictureMapper
+//
+//  Created by Jordan Zucker on 1/23/15.
+//  Copyright (c) 2015 Jordan Zucker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+@end
+
---
+++ RunnerPictureMapper/RunnerPictureMapper/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  RunnerPictureMapper
+//
+//  Created by Jordan Zucker on 1/23/15.
+//  Copyright (c) 2015 Jordan Zucker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
+
